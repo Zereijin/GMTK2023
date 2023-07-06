15523082f7bfd15ffa888499c91ff2d9f51f4b4c
Zereijin <zereijin@gmail.com>
2023-07-05 17:20:48 -0700

    Bootstrap

---
+++ main_menu/main_menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+@export var start_scene: PackedScene
+var mainMenuFocusButton : Button
+
+func _ready():
+	mainMenuFocusButton = $MainMenuButtonContainer/PlayButton
+	mainMenuFocusButton.grab_focus()
+
+func _on_play_button_pressed():
+	var error := get_tree().change_scene_to_packed(start_scene)
+	if error != OK:
+		print(error)
+		get_tree().quit(1)
+
+func _on_credits_button_pressed():
+	mainMenuFocusButton = $MainMenuButtonContainer/CreditsButton
+	get_node("CreditPanel").visible=true
+	$CreditPanel/MarginContainer/VBoxContainer/CloseCreditsButton.grab_focus()
+
+func _on_close_credits_button_pressed():
+	get_node("CreditPanel").visible=false
+	mainMenuFocusButton.grab_focus()
+
+func _on_quit_button_pressed():
+	get_tree().quit()
