869ed70a19118042aabb14d41d7648baf5520e77
Christopher Head <chead@chead.ca>
2023-07-08 11:16:23 -0700

    Implement end-of-game screen

---
+++ end_scene/end_scene.gd
@@ -0,0 +1,14 @@
+class_name EndScene
+extends VBoxContainer
+
+func go(scores: PackedInt64Array, my_score_pos: int) -> void:
+	for i in range(scores.size()):
+		var text := str(scores[i])
+		if i == my_score_pos:
+			text += " <<<"
+		$scores.get_node("score" + str(i)).text = text
+	visible = true
+
+func _on_ok_button_pressed() -> void:
+	print("OK pressed")
+	get_tree().change_scene_to_file("res://main_menu/main_menu.tscn")
--- game_scene/game_scene.gd
+++ game_scene/game_scene.gd
@@ -1,1 +1,6 @@
 extends Node2D
+
+func _on_game_over() -> void:
+	get_tree().paused = true
+	var low_score_data := PlayerData.end_game()
+	$hud/end_scene.go(low_score_data[0], low_score_data[1])
