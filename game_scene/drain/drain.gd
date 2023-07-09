123c552c9472c861ba3580df1a1827db7a63e48b
Christopher Head <chead@chead.ca>
2023-07-08 21:23:29 -0700

    Fire drain event on last ball
    This allows the confetti cannonade to fire.

--- game_scene/drain/drain.gd
+++ game_scene/drain/drain.gd
@@ -3,7 +3,7 @@ extends Area2D
 ## Emitted when the game is over.
 signal game_over()
 
-## Emitted when a ball is drained and the game is not over.
+## Emitted when a ball is drained.
 signal drained()
 
 ## The ball.
@@ -23,12 +23,10 @@ func _on_body_entered(body: Node2D) -> void:
 	assert(ball != null)
 	ball.sensitive = false
 	ball.sleeping = true
-	var another := PlayerData.use_ball()
-	if another:
-		drained.emit()
+	drained.emit()
 	_audio.play()
 	await _audio.finished
-	if another:
+	if PlayerData.use_ball():
 		PlayerData.first_launch = true
 		ball.position = _initial_ball_position
 		ball.linear_velocity = Vector2.ZERO
