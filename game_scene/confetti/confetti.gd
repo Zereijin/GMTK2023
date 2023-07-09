7795e5195c92766919500660e6f43583767917bc
Aaron Lau <projects@lau-aa.ro>
2023-07-08 19:46:13 -0700

    Defined confetti fire method

---
+++ game_scene/confetti/confetti.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func fire():
+	$blueGPUParticles2D.emitting = true
+	$cyanGPUParticles2D.emitting = true
+	$greenGPUParticles2D.emitting = true
+	$magentaGPUParticles2D.emitting = true
+	$redGPUParticles2D.emitting = true
+	$yellowGPUParticles2D.emitting = true
